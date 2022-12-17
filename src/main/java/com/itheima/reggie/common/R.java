498e649c603731192b5e631dc999859c15b38dd6
tonymarsh <tonymarsh0331@gmail.com>
2022-12-17 16:03:54 +0800

    :beers: 一个通用的返回结果类

---
+++ src/main/java/com/itheima/reggie/common/R.java
@@ -0,0 +1,46 @@
+package com.itheima.reggie.common;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 一个通用的返回结果类 R stand for Return/Result，服务端返回的数据都用该类封装
+ *
+ * @param <T>
+ */
+@Data
+public class R<T> {
+
+    /**
+     * 编码：1成功，0和其它数字为失败
+     */
+    private Integer code;
+
+    private String msg; //错误信息
+
+    private T data; //数据
+
+    private Map<String, Object> map = new HashMap<>(); //动态数据
+
+    public static <T> R<T> success(T object) {
+        R<T> r = new R<>();
+        r.data = object;
+        r.code = 1;
+        return r;
+    }
+
+    public static <T> R<T> error(String msg) {
+        R<T> r = new R<>();
+        r.msg = msg;
+        r.code = 0;
+        return r;
+    }
+
+    public R<T> add(String key, Object value) {
+        this.map.put(key, value);
+        return this;
+    }
+
+}
