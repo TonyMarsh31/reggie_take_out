3f190a954937b6b80b3e6c7fbac16733d6702a7c
tonymarsh <tonymarsh0331@gmail.com>
2022-12-21 19:34:32 +0800

    :beers: 配置SpringMVC的消息转换器，使其使用自定义的Jackson对象转换器

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.reggie.config;
 
+import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -18,5 +23,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("配置静态资源映射");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 配置消息转换器
+     *
+     * @param converters 消息转换器列表
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建一个新的消息转换器
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象映射器，用于将对象转换为json字符串，使用的是自定义的重写的jackson（配置其将Long型数据转为String处理，以避免js对Long型数据处理时的精度丢失）
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将新的消息转换器添加到消息转换器列表中,并将其放在第一位,以防止仍然使用默认的消息转换器
+        converters.add(0, mappingJackson2HttpMessageConverter);
     }
 }
