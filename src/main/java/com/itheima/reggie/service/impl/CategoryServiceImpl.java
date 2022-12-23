d1faef9c99e71b0c6ba9a2e205a9c9f136269dd2
tonymarsh <tonymarsh0331@gmail.com>
2022-12-23 18:42:55 +0800

    :beers: 完善分类删除功能，加入嵌套属性检查，如果该分类下已有菜品或套餐则无法进行删除

--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,49 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.exception.ObjectContainsNestedProperties;
 import com.itheima.reggie.mapper.CategoryMapper;
 import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.DishService;
+import com.itheima.reggie.service.SetmealService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 
+    private final DishService dishService;
+    private final SetmealService setmealService;
+
+    public CategoryServiceImpl(DishService dishService, SetmealService setmealService) {
+        this.dishService = dishService;
+        this.setmealService = setmealService;
+    }
+
+    /**
+     * 根据id删除分类，删除前进行判断:如果分类下有菜品或套餐，不允许删除
+     *
+     * @param id 分类id
+     */
+    @Override
+    public void remove(Long id) {
+        //先查询该分类下是否有菜品或套餐
+        LambdaQueryWrapper<Dish> dishQueryWrapper = new LambdaQueryWrapper<>();
+        dishQueryWrapper.eq(Dish::getCategoryId, id);
+        int count = dishService.count(dishQueryWrapper);
+        if (count > 0) {
+            throw new ObjectContainsNestedProperties("该分类下已有菜品，无法进行删除");
+        }
+        LambdaQueryWrapper<Setmeal> setmealQueryWrapper = new LambdaQueryWrapper<>();
+        setmealQueryWrapper.eq(Setmeal::getCategoryId, id);
+        int count1 = setmealService.count(setmealQueryWrapper);
+        if (count1 > 0) {
+            throw new ObjectContainsNestedProperties("该分类下已有套餐，无法进行删除");
+        }
+        //如果没有，就删除
+        baseMapper.deleteById(id);
+    }
 }
