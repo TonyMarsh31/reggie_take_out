ffcce96400e67b745f0ca06d242504079fb0d4df
tonymarsh <tonymarsh0331@gmail.com>
2022-12-24 18:03:56 +0800

    :beers: 菜品部分的MVC基本架构：Mapper、Service、Controller

---
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -0,0 +1,28 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 菜品管理
+ */
+@RestController
+@RequestMapping("/dish")
+@Slf4j
+public class DishController {
+    private final DishService dishService;
+    private final DishFlavorService dishFlavorService;
+    private final CategoryService categoryService;
+
+
+    public DishController(DishService dishService, DishFlavorService dishFlavorService, CategoryService categoryService) {
+        this.dishService = dishService;
+        this.dishFlavorService = dishFlavorService;
+        this.categoryService = categoryService;
+    }
+
+}
