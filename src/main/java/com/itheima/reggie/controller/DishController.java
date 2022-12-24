14da51c8ccca319fb3658baae50c5b8e0b73a60e
tonymarsh <tonymarsh0331@gmail.com>
2022-12-25 00:28:46 +0800

    :beers: 完成菜品新增功能

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,9 +1,13 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +29,10 @@ public class DishController {
         this.categoryService = categoryService;
     }
 
+    @PostMapping
+    public R<String> save(@RequestBody DishDto dishDto) {
+        log.info("dishDto:{}", dishDto);
+        dishService.saveWithFlavor(dishDto);
+        return R.success("新增成功");
+    }
 }
