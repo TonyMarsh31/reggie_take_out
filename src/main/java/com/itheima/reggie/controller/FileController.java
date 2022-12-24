ec29f3b7238e67166fd74a00b1ae61272223f7ea
tonymarsh <tonymarsh0331@gmail.com>
2022-12-24 16:39:05 +0800

    :beers: 实现文件下载功能

--- src/main/java/com/itheima/reggie/controller/FileController.java
+++ src/main/java/com/itheima/reggie/controller/FileController.java
@@ -2,11 +2,15 @@ package com.itheima.reggie.controller;
 
 import com.itheima.reggie.common.R;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
@@ -34,5 +38,32 @@ public class FileController {
             throw new RuntimeException(e);
         }
         return R.success(newFileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param name     文件名
+     * @param response HttpServlet响应
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        //  先使用输入流读取对应文件到内存，然后使用输出流写入到response中返回给客户端浏览器
+        try {
+            FileInputStream fileInputStream = new FileInputStream(UPLOAD_PATH + name);
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            // todo: 以下copy流的操作可以使用common io工具类优化
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
