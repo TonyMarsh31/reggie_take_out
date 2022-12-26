5f1690cb5ca45f29d0e41df89b06590c35f9050c
tonymarsh <tonymarsh0331@gmail.com>
2022-12-26 19:51:32 +0800

    :beers: 完成套餐数据的分页查询

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -1,15 +1,21 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
 import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.SetmealDishService;
 import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -35,5 +41,32 @@ public class SetmealController {
         log.info("套餐信息：{}", setmealDto);
         setmealService.saveWithDish(setmealDto);
         return R.success("新增套餐成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page<SetmealDto>> page(int page, int pageSize, String name) {
+        // 查询套餐的基础信息
+        Page<Setmeal> setmealPage = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper
+                .like(StringUtils.isNotBlank(name), Setmeal::getName, name)
+                .orderByDesc(Setmeal::getUpdateTime);
+        setmealService.page(setmealPage, queryWrapper);
+        //将setmealPage转换为setmealDtoPage,添加分类名称与嵌套的菜品信息
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+        BeanUtils.copyProperties(setmealPage, setmealDtoPage, "records");
+        List<SetmealDto> setmealDtoRecords = setmealPage.getRecords().stream()
+                .map(setmeal ->
+                {
+                    SetmealDto setmealDto = new SetmealDto();
+                    BeanUtils.copyProperties(setmeal, setmealDto);
+                    setmealDto.setCategoryName(setmeal.getCategoryId() == null ? "无数据" : categoryService.getById(setmeal.getCategoryId()).getName());
+                    LambdaQueryWrapper<SetmealDish> eq = new LambdaQueryWrapper<SetmealDish>().eq(SetmealDish::getSetmealId, setmeal.getId());
+                    setmealDto.setSetmealDishes(setmealDishService.list(eq));
+                    return setmealDto;
+                })
+                .collect(Collectors.toList());
+        setmealDtoPage.setRecords(setmealDtoRecords);
+        return R.success(setmealDtoPage);
     }
 }
