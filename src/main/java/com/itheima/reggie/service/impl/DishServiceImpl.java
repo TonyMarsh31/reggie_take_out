e36a9bfd0030b4b4490f47b92cde3f209fa9cafa
tonymarsh <tonymarsh0331@gmail.com>
2022-12-27 18:27:54 +0800

    :beers: 完成TODO，完善菜品起售、停售功能，现在当一个菜品存在于一个起售的套餐中时，无法进行停售

--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -5,9 +5,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
+import com.itheima.reggie.exception.ObjectStillOnStockException;
 import com.itheima.reggie.mapper.DishMapper;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -20,9 +25,13 @@ import java.util.List;
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
 
     private final DishFlavorService dishFlavorService;
+    private final SetmealService setmealService;
+    private final SetmealDishService setmealDishService;
 
-    public DishServiceImpl(DishFlavorService dishFlavorService) {
+    public DishServiceImpl(DishFlavorService dishFlavorService, SetmealService setmealService, SetmealService setmealService1, SetmealDishService setmealDishService) {
         this.dishFlavorService = dishFlavorService;
+        this.setmealService = setmealService1;
+        this.setmealDishService = setmealDishService;
     }
 
 
@@ -82,12 +91,32 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements Di
     /**
      * 修改菜品的销售状态：起售、停售
      *
-     * @param status 销售状态
+     * @param status 销售状态, 0停售，1起售
      * @param ids    菜品id集合
      */
     @Override
     public void updateStatus(Integer status, List<Long> ids) {
-        //todo 需要完善的地方：当菜品在一个起售中的套餐中时，无法进行停售操作，提示用户需要先停售套餐
+        // 当菜品在一个起售中的套餐中时，无法进行停售操作，提示用户需要先停售套餐
+
+        // 1. 查询套餐表中正在起售的套餐
+        // 2. 根据起售的套餐id查询 套餐菜品关联表，得到所有的在起售套餐中的菜品id
+        // 3. 判断：如果前端传递的菜品id中包含了在起售套餐中的菜品id，则说明这个菜品在起售中的套餐中，无法进行停售操作
+        if (status == 0) {
+            LambdaQueryWrapper<Setmeal> setmealOnSale = new LambdaQueryWrapper<>();
+            setmealOnSale.eq(Setmeal::getStatus, 1);
+            setmealService.list(setmealOnSale).stream().map(Setmeal::getId).forEach(setmealId -> {
+                LambdaQueryWrapper<SetmealDish> setmealDishOnSale = new LambdaQueryWrapper<>();
+                setmealDishOnSale.eq(SetmealDish::getSetmealId, setmealId);
+                List<SetmealDish> onSaleSetmealDish = setmealDishService.list(setmealDishOnSale);
+                onSaleSetmealDish.stream().map(SetmealDish::getDishId).forEach(onSaleDishID -> {
+                    if (ids.contains(onSaleDishID)) {
+                        throw new ObjectStillOnStockException("存在菜品在起售中的套餐中，无法进行停售操作");
+                    }
+                });
+            });
+        }
+
+        //正常的停售与起售菜品操作
         Dish newStatus = new Dish();
         newStatus.setStatus(status);
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
