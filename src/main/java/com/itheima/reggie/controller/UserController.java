447232b9def8af0e370bb5dd5bb54073b19ba2d1
tonymarsh <tonymarsh0331@gmail.com>
2022-12-28 23:50:45 +0800

    :beers: Controller完成用户登录与注册相关功能

---
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -0,0 +1,84 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.User;
+import com.itheima.reggie.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 发送手机短信验证码
+     *
+     * @param user 前端信息包装类，包含手机号
+     */
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
+        //获取手机号
+        String phone = user.getPhone();
+        if (StringUtils.isNotEmpty(phone)) {
+            //TODO : 无法解决SMS服务问题
+            // 原定设计是 1.生成随机4位校验码 2.调用短信服务API将校验码通过短信发送给注册/登录手机号
+            // 但中国境内的SMS服务已经不再为个人用户提供服务，仅面向认证企业对象开放API
+
+            // 在此测试环境下，生成静态校验码1234，校验码无法通过短信发送给注册/登录手机号
+            String code = "1234";
+            //需要将生成的验证码保存到Session
+            session.setAttribute(phone, code);
+            return R.success("手机验证码短信发送成功");
+        }
+        return R.error("短信发送失败");
+    }
+
+    /**
+     * 移动端用户登录
+     *
+     * @param args 前段传递的参数，包含手机号和验证码
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map args, HttpSession session) {
+        log.info(args.toString());
+        //获取手机号
+        String phone = args.get("phone").toString();
+        //获取验证码
+        String code = args.get("code").toString();
+        //从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+        //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
+        if (codeInSession != null && codeInSession.equals(code)) {
+            //如果能够比对成功，说明登录成功
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null) {
+                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user", user.getId());
+            return R.success(user);
+        }
+        return R.error("登录失败");
+    }
+
+}
