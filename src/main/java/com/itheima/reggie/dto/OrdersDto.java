ca5b8dd7597c722a8af346a844931e2949061bb1
tonymarsh <tonymarsh0331@gmail.com>
2022-12-30 03:21:38 +0800

    :beers: 完成后台查看订单明细

---
+++ src/main/java/com/itheima/reggie/dto/OrdersDto.java
@@ -0,0 +1,24 @@
+package com.itheima.reggie.dto;
+
+import com.itheima.reggie.entity.OrderDetail;
+import com.itheima.reggie.entity.Orders;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class OrdersDto extends Orders {
+
+    private String userName;
+
+    private String phone;
+
+    private String address;
+
+    private String consignee;
+
+    private List<OrderDetail> orderDetails;
+
+}
