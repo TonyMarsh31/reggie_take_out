25b2d50d685f585c73265ee63050a9c1b11e60b6
tonymarsh <tonymarsh0331@gmail.com>
2022-12-29 18:31:40 +0800

    :beers: 初步实现用户地址簿管理

---
+++ src/main/java/com/itheima/reggie/controller/AddressBookController.java
@@ -0,0 +1,96 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.AddressBook;
+import com.itheima.reggie.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 地址簿管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/addressBook")
+public class AddressBookController {
+
+    private final AddressBookService addressBookService;
+
+    public AddressBookController(AddressBookService addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+
+    //TODO 完成地址更新 和删除功能的实现
+
+    /**
+     * 新增收货地址
+     */
+    @PostMapping
+    public R<AddressBook> save(@RequestBody AddressBook addressBook) {
+        // 前端传递的dto缺少用户绑定，此处手动绑定用户ID
+        addressBook.setUserId(BaseContext.getCurrentId());
+        log.info("addressBook:{}", addressBook);
+        addressBookService.save(addressBook);
+        return R.success(addressBook);
+    }
+
+    /**
+     * 设置默认收货地址
+     */
+    @PutMapping("default")
+    public R<AddressBook> setDefault(@RequestBody AddressBook addressBook) {
+        // 1.将当前用户的所有地址设置为非默认 2.将传递的地址设置为默认
+        log.info("addressBook:{}", addressBook);
+        LambdaUpdateWrapper<AddressBook> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
+        wrapper.set(AddressBook::getIsDefault, 0);
+        //SQL:update address_book set is_default = 0 where user_id = ?
+        addressBookService.update(wrapper);
+        addressBook.setIsDefault(1);
+        //SQL:update address_book set is_default = 1 where id = ?
+        addressBookService.updateById(addressBook);
+        return R.success(addressBook);
+    }
+
+    /**
+     * 根据id查询地址
+     */
+    @GetMapping("/{id}")
+    public R<AddressBook> get(@PathVariable Long id) {
+        AddressBook addressBook = addressBookService.getById(id);
+        return addressBook != null ? R.success(addressBook) : R.error("没有找到该对象");
+    }
+
+    /**
+     * 查询默认地址
+     */
+    @GetMapping("default")
+    public R<AddressBook> getDefault() {
+        LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
+        queryWrapper.eq(AddressBook::getIsDefault, 1);
+        //SQL:select * from address_book where user_id = ? and is_default = 1
+        AddressBook addressBook = addressBookService.getOne(queryWrapper);
+        return addressBook != null ? R.success(addressBook) : R.error("没有找到该对象");
+    }
+
+    /**
+     * 查询指定用户的全部地址
+     */
+    @GetMapping("/list")
+    public R<List<AddressBook>> list(AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        log.info("addressBook:{}", addressBook);
+        //条件构造器
+        LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(null != addressBook.getUserId(), AddressBook::getUserId, addressBook.getUserId());
+        queryWrapper.orderByDesc(AddressBook::getUpdateTime);
+        //SQL:select * from address_book where user_id = ? order by update_time desc
+        return R.success(addressBookService.list(queryWrapper));
+    }
+}
