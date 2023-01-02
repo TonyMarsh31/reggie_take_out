966a2e026692c275b5cf0b7102507f30f978c28d
tonymarsh <tonymarsh0331@gmail.com>
2023-01-02 19:18:02 +0800

    :art: 使用springData-Redis + springCache 简化缓存方面的代码，序列化R以使得其能写入Redis

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -6,7 +6,8 @@ import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,75 +20,34 @@ import java.util.List;
 @Slf4j
 public class DishController {
     private final DishService dishService;
-    private final RedisTemplate<Object, Object> redisTemplate;
 
-    public DishController(DishService dishService, RedisTemplate<Object, Object> redisTemplate) {
+    public DishController(DishService dishService) {
         this.dishService = dishService;
-        this.redisTemplate = redisTemplate;
     }
 
+    //region 管理端API
+    // 后台操作并发量不大，不需要缓存，但在CRUD操作后需要清除缓存
+
+    /**
+     * 管理端添加新的菜品，同时向菜品和菜品口味表中添加数据
+     * <p>
+     * 添加完成后，清除该分类下的菜品Redis缓存
+     *
+     * @param dishDto 菜品和口味信息
+     */
+    @CacheEvict(value = "dishListByCategory", key = "#dishDto.categoryId")
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto) {
         dishService.saveWithFlavor(dishDto);
-        // mysql数据库中的数据发生变化，删除旧的缓存数据
-        String key = "dish_" + dishDto.getCategoryId() + "_1"; //_1 是status,表示菜品状态为上架
-        redisTemplate.delete(key);
         return R.success("新增成功");
     }
 
-    @PutMapping
-    public R<String> update(@RequestBody DishDto dishDto) {
-        dishService.updateWithFlavor(dishDto);
-        // mysql数据库中的数据发生变化，删除旧的缓存数据
-        String key = "dish_" + dishDto.getCategoryId() + "_1";
-        redisTemplate.delete(key);
-        return R.success("修改成功");
-    }
-
-    @GetMapping("/page")
-    public R<Page<DishDto>> page(int page, int pageSize, String name) {
-        Page<Dish> dishPage = new Page<>(page, pageSize);
-        dishService.getDishPage(dishPage, name);
-        Page<DishDto> dishDtoPage = dishService.convertToDishDtoPage(dishPage);
-        return R.success(dishDtoPage);
-    }
-
-    @GetMapping("/{id}")
-    public R<DishDto> getById(@PathVariable Long id) {
-        DishDto dishDto = dishService.getDishDtoWithFlavorById(id);
-        return R.success(dishDto);
-    }
-
     /**
-     * 根据分类查询先关菜品信息
-     * <p>
-     * 先从Redis中获取缓存数据，存在则直接返回，没有则查询mysql，然后缓存并返回
+     * 管理端删除菜品信息，同时删除菜品和菜品口味表中的数据
      *
-     * @param condition 封装的查询条件
+     * @param ids 菜品id
      */
-    @GetMapping("/list")
-    public R<List<DishDto>> list(Dish condition) {
-        //动态构造key  示例：dish_1397844391040167938_1
-        String key = "dish_" + condition.getCategoryId() + "_" + condition.getStatus();
-        List<DishDto> dishes = (List<DishDto>) redisTemplate.opsForValue().get(key);
-        // Redis中存在数据，直接返回
-        if (dishes != null) return R.success(dishes);
-        // Redis中没有缓存，查询mysql
-        List<Dish> dishListByCategory = dishService.getDishListByCategory(condition);
-        // 将Dish对象转换为DishDto对象,其额外添加了口味数据
-        List<DishDto> dishDtoList = dishService.convertToDishDtoList(dishListByCategory);
-        // 将数据缓存到Redis中
-        //TODO 需要设置过期时间吗？
-        redisTemplate.opsForValue().set(key, dishDtoList);
-        return R.success(dishDtoList);
-    }
-
-    @PostMapping("/status/{status}")
-    public R<String> updateStatus(@PathVariable Integer status, @RequestParam List<Long> ids) {
-        dishService.updateStatus(status, ids);
-        return R.success("修改成功");
-    }
-
+    @CacheEvict(value = "dishListByCategory", allEntries = true)
     @DeleteMapping
     public R<String> delete(@RequestParam List<Long> ids) {
         // 需要进行逻辑删除的信息有：菜品信息、菜品口味信息
@@ -95,4 +55,72 @@ public class DishController {
         dishService.deleteWithFlavor(ids);
         return R.success("删除套餐成功");
     }
+
+    /**
+     * 管理端更新菜品信息，同时更新菜品和菜品口味表中的数据
+     *
+     * @param dishDto 菜品和口味信息
+     */
+    @CacheEvict(value = "dishListByCategory", key = "#dishDto.categoryId")
+    @PutMapping
+    public R<String> update(@RequestBody DishDto dishDto) {
+        dishService.updateWithFlavor(dishDto);
+        return R.success("修改成功");
+    }
+
+    /**
+     * 管理端更改菜品的状态： 起售|停售
+     *
+     * @param status 状态
+     * @param ids    菜品id
+     */
+    @CacheEvict(value = "dishListByCategory", allEntries = true)
+    @PostMapping("/status/{status}")
+    public R<String> updateStatus(@PathVariable Integer status, @RequestParam List<Long> ids) {
+        dishService.updateStatus(status, ids);
+        return R.success("修改成功");
+    }
+
+    /**
+     * 管理端根据主键查询菜品详细信息，一般是在修改菜品信息时使用
+     *
+     * @param id 菜品id
+     */
+    @GetMapping("/{id}")
+    public R<DishDto> getById(@PathVariable Long id) {
+        DishDto dishDto = dishService.getDishDtoWithFlavorById(id);
+        return R.success(dishDto);
+    }
+
+    /**
+     * 管理端分页查询菜品信息
+     */
+    @GetMapping("/page")
+    public R<Page<DishDto>> page(int page, int pageSize, String name) {
+        Page<Dish> dishPage = new Page<>(page, pageSize);
+        dishService.getDishPage(dishPage, name);
+        Page<DishDto> dishDtoPage = dishService.convertToDishDtoPage(dishPage);
+        return R.success(dishDtoPage);
+    }
+    //endregion
+
+    //region 用户端API
+
+    /**
+     * 用户端根据分类查询相关菜品信息
+     * <p>
+     * 先从Redis中获取缓存数据，存在则直接返回，没有则查询mysql，然后缓存并返回
+     *
+     * @param condition 封装的查询条件
+     */
+    @Cacheable(value = "dishListByCategory", key = "#condition.categoryId")
+    @GetMapping("/list")
+    public R<List<DishDto>> list(Dish condition) {
+        List<Dish> dishListByCategory = dishService.getDishListByCategory(condition);
+        // 将Dish对象转换为DishDto对象,其额外添加了口味数据
+        List<DishDto> dishDtoList = dishService.convertToDishDtoList(dishListByCategory);
+        return R.success(dishDtoList);
+    }
+
+    //endregion
 }
