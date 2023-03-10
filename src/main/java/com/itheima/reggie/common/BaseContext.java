775610fe70c68b6742a717b6fd647655f85dd1ef
tonymarsh <tonymarsh0331@gmail.com>
2023-03-11 00:17:57 +0800

    :memo: 添加注释

--- src/main/java/com/itheima/reggie/common/BaseContext.java
+++ src/main/java/com/itheima/reggie/common/BaseContext.java
@@ -2,6 +2,8 @@ package com.itheima.reggie.common;
 
 /**
  * 基于ThreadLocal封装工具类，用户保存和获取当前登录用户id
+ * ThreadLocal是Java中一个线程级别的变量，每个线程都可以存储一个ThreadLocal数据副本，该副本仅由该线程读取和写入，并且在其它线程中不可见。
+ * 在本场景下，需要将一些用户id在多个方法之间传递。使用ThreadLocal可以在当前线程中携带这些变量，不必担心方法间如何传递它们。
  */
 public class BaseContext {
     private static final ThreadLocal<Long> threadLocal = new ThreadLocal<>();
